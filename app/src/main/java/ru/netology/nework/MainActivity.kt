eea97ad1b551cee5c00521eab1ab5d93908a653d
AnTomS <thekeibee@yandex.ru>
2022-12-27 20:22:10 +0800

    Диплом. commit #2 Руссификация на нижнюю панель + MaterialDesign3

--- app/src/main/java/ru/netology/nework/MainActivity.kt
+++ app/src/main/java/ru/netology/nework/MainActivity.kt
@@ -19,8 +19,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //получаем доступ к BottomNavigationView
         val navView: BottomNavigationView = binding.navView
 
+        //получаем доступ к фрагменту, в который будем вставлять другие фрагменты, через findNavController
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
