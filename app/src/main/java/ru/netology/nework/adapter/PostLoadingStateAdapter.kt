773760696e91920c9157378b919bdca5bbd043dc
AnTomS <thekeibee@yandex.ru>
2023-01-01 21:35:34 +0800

    Диплом. commit #5 Внедрение зависимостей

---
+++ app/src/main/java/ru/netology/nework/adapter/PostLoadingStateAdapter.kt
@@ -0,0 +1,34 @@
+package ru.netology.nework.adapter
+
+class PostLoadingStateAdapter (
+    private val retryListener: ()->Unit
+) : LoadStateAdapter<PostLoadingViewHolder>() {
+    override fun onBindViewHolder(holder: PostLoadingViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ): PostLoadingViewHolder =
+        PostLoadingViewHolder(
+            ItemLoadingBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            retryListener,
+        )
+}
+
+
+class PostLoadingViewHolder(
+    private val itemLoadingBinding: ItemLoadingBinding,
+    private val retryListener: ()->Unit
+) : RecyclerView.ViewHolder(itemLoadingBinding.root) {
+    fun bind(loadState: LoadState) {
+        itemLoadingBinding.apply {
+            progress.isVisible = loadState is LoadState.Loading
+            retryButton.isVisible = loadState is LoadState.Error
+            retryButton.setOnClickListener{
+                retryListener()
+            }
+        }
+    }
+}
