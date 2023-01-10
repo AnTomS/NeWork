8309ac86638554623db85f6c238d2c2cd779d684
AnTomS <thekeibee@yandex.ru>
2023-01-10 09:16:44 +0800

    Диплом. Ветка 1 commit #8 краш при добавлении постов

--- app/src/main/java/ru/netology/nework/AppActivity.kt
+++ app/src/main/java/ru/netology/nework/AppActivity.kt
@@ -5,21 +5,25 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.messaging.FirebaseMessaging
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import ru.netology.nework.auth.AppAuth
-import ru.netology.nework.databinding.ActivityMainBinding
 import ru.netology.nework.viewmodel.AuthViewModel
 import javax.inject.Inject
 
@@ -39,56 +43,77 @@ class AppActivity : AppCompatActivity() {
         private const val MAPKIT_API_KEY = "4020182c-79bf-4ab9-b30b-08aaddd23117"
     }
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: Act
 
     @SuppressLint("ResourceType")
-    fun setActionBarTitle(title: String) {
-        binding.mainToolbar.title = title
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        val bottomNavigation = binding.bottomNavView
 
-                menuInflater.inflate(R.menu.fragment_create_edit_menu, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return true
-            }
-        })
-
-        //получаем доступ к BottomNavigationView
-        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
-
-        //получаем доступ к фрагменту в файле xml, в который будем вставлять фрагменты, через supportFragmentManager
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController: NavController = navHostFragment.navController
 
-        navView.setupWithNavController(navController)
-
-        val toolbar = binding.mainToolbar
-        setSupportActionBar(toolbar)
-
-        binding.fabAddPost.setOnClickListener {
-            navController.navigate(R.id.newPostFragment)
+        val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.PostsFragment, R.id.eventFeedFragment, R.id.contactsFragment, R.id.userProfileFragment -> bottomNavigation.visibility =
+                    View.VISIBLE
+                else -> bottomNavigation.visibility = View.GONE
+            }
         }
+        val appBar = AppBarConfiguration(
+            setOf(
+                R.id.postFeedFragment,
+                R.id.eventFeedFragment,
+                R.id.contactsFragment,
+                R.id.userProfileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBar)
+        bottomNavigation.setupWithNavController(navController)
 
 
+        viewModel.data.observe(this) {
+            invalidateOptionsMenu()
+        }
         checkGoogleApiAvailability()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_authorization, menu)
+
+        menu.let {
+            it.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
+            it.setGroupVisible(R.id.authenticated, viewModel.authenticated)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.signin -> {
+                findNavController(R.id.navHostFragment).navigate(R.id.signInFragment)
+                true
+            }
+            R.id.signup -> {
+                findNavController(R.id.navHostFragment).navigate(R.id.registerFragment)
+                true
+            }
+            R.id.signout -> {
+                appAuth.removeAuth()
+                findNavController(R.id.navHostFragment).navigateUp()
+                true
+            }
+            else -> false
+        }
+    }
 
     private fun checkGoogleApiAvailability() {
-        with(GoogleApiAvailability.getInstance()) {
+        with(googleApiAvailability) {
             val code = isGooglePlayServicesAvailable(this@AppActivity)
             if (code == ConnectionResult.SUCCESS) {
                 return@with
@@ -97,12 +122,15 @@ class AppActivity : AppCompatActivity() {
                 getErrorDialog(this@AppActivity, code, 9000)?.show()
                 return
             }
-            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG)
+            Toast.makeText(this@AppActivity,
+                R.string.google_api_unavailable_message,
+                Toast.LENGTH_LONG)
                 .show()
         }
+    }
 
-        FirebaseMessaging.getInstance().token.addOnSuccessListener {
-            println(it)
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
